beb10d8dd3f03ae2c47ac2c8586bd1f35037d358
Andre Lucas <alucas748@gmail.com>
2022-12-27 02:35:14 -0300

    Interface do projeto criada

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,44 @@ class _HomeState extends State<Home> {
         title: const Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
+      body: Center(
+        child: Container(
         padding: const EdgeInsets.all(16),
+        /*//width: double.infinity,
         decoration: BoxDecoration(
           border: Border.all(width: 3, color: Colors.amber)
+        ),*/
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo.png"),
+            const Text(
+              "Clique abaixo para gerar uma frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 25,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green)
+                ),
+                child: const Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                ),
+          ],
         ),
+      ),
       ),
     );
   }
